562a302f6f27381ceca38f75ee1ed22aeadf0120
양희태 <ht@yanghuitaeui-MacBookPro.local>
2024-03-18 23:31:34 +0900

    Feat : 사진 기능 보완 및 메모 기능 추가

--- Mobile/상추 - 상권추천서비스/Core/Views/BookMark/UpdateBookMark.swift
+++ Mobile/상추 - 상권추천서비스/Core/Views/BookMark/UpdateBookMark.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import SwiftData
 import PhotosUI // 사진 선택기를 가져오기 위함
+import SwiftUIImageViewer
 
 struct UpdateBookMarkView: View {
     
@@ -15,32 +16,52 @@ struct UpdateBookMarkView: View {
     @Query private var hashtags : [Hashtag]
     @State var selectedHashtag : Hashtag?
     
+    @State private var isImageViewerPresented = false
     @State var selectedPhoto : PhotosPickerItem?
     @Bindable var item: BookMarkItem
     var body: some View {
         List {
-            Section(header: Text("상권정보 추가")){
-                TextField("상권명", text: $item.cdTitle)
-                TextField("상권정보", text: $item.cdInfo)
+            Section(header: Text("상권정보")){
+                Text("상권명 : \(item.cdTitle)")
+                Text("상권정보 :  \(item.cdInfo)")
             }
-//            Toggle("중요한가요?", isOn: $item.isImportant)
-            Section(header: Text("해시태그 추가")){
-                Picker("",selection: $selectedHashtag){
-                    ForEach(hashtags) { hashtag in
-                        Text(hashtag.title).tag(hashtag as Hashtag?)
+                Section(header: Text("해시태그 추가")){
+                    Picker("선택된 태그",selection: $selectedHashtag){
+                        ForEach(hashtags) { hashtag in
+                            Text(hashtag.title).tag(hashtag as Hashtag?)
+                        }
+                        Text("없음").tag(nil as Hashtag?)
                     }
-                    Text("없음").tag(nil as Hashtag?)
                 }
-            }.labelsHidden().pickerStyle(.inline)
             
-            Section(header : Text("메모")){
-                TextField("메모", text: $item.userMemo)
+            
+            Section(header: Text("메모")) {
+                ZStack(alignment: .topLeading) {
+                    if item.userMemo.isEmpty {
+                        Text("메모를 입력하세요")
+                            .foregroundColor(.gray)
+                            .padding(.top, 8)
+                            .padding(.leading, 4)
+                    }
+                    TextEditor(text: $item.userMemo)
+                }
             }
-            
             Section {
                 
                 if let iamgeData = item.image, let uiImage = UIImage(data: iamgeData){
                     Image(uiImage: uiImage).resizable().scaledToFit().frame(maxWidth: .infinity,maxHeight: 300)
+                        .onTapGesture {
+                            isImageViewerPresented = true
+                        }
+                        .fullScreenCover(isPresented: $isImageViewerPresented){
+                            SwiftUIImageViewer(image:Image(uiImage: uiImage)).overlay(alignment: .topTrailing){
+                                Button{
+                                    isImageViewerPresented = false
+                                } label: {
+                                    Image(systemName: "xmark").font(.headline)
+                                }.buttonStyle(.bordered).clipShape(Circle()).tint(.purple).padding()
+                            }
+                        }
                 }
                 
                 PhotosPicker(selection : $selectedPhoto,
@@ -67,14 +88,8 @@ struct UpdateBookMarkView: View {
                 dismiss()
             }
         }
-        .navigationTitle("수정하기")
+        .navigationTitle("메모")
         .toolbar{
-            ToolbarItem(placement: .cancellationAction){
-                Button("뒤로가기"){
-                    dismiss()
-                }
-            }
-            
             ToolbarItem(placement: .primaryAction){
                 Button("완료"){
                     dismiss()
