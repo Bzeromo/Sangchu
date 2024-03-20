aaedd18bca4e6dee05084f0b12c44826520ab556
안성재 <sammy0329@naver.com>
2024-03-20 16:03:04 +0900

    Feat: RDI, 상권 내 전체 점포밀도 구하는 모듈 성능 최적화

--- Backend/DataPreProcessing/modules/calc_RDI.py
+++ Backend/DataPreProcessing/modules/calc_RDI.py
@@ -1,115 +1,48 @@
-import pandas as pd
-import math
-from tqdm import tqdm
-
-# 논문 1번 업종 다양성 구하기 위해
-# Sj = j 업종 점포 개수/서울시 총 업종 점포 개수 -> 서울시 전체에서 업종 j가 차지하는 비율
-# Sij = i 상권 내 j 업종 점포 개수 / i 상권의 총 점포 개수
-
-category_codes = {
-    'C0101' : "음식(식사)",
-    'C0102' : "제과/커피",
-    'C0103' : "호프간이주점",
-    'C0201' : "학원",
-    'C0202' : "의원",
-    'C0203' : "여가/미용",
-    'C0204' : "기타 서비스",
-    'C0301' : "생활소매",
-    'C0302' : "전자소매",
-    'C0303' : "잡화소매"
-}
-
 def calc_RDI(seoul_df, store_df, area_df, result_df):
-    # ['기준_년분기_코드', '중분류_코드', '서울_중분류_코드_합', '서울_seoul_middle_category_code_ratio_to_the_entire_industry_ratio']
-    df = pd.DataFrame(columns=['year_quarter_code', 'middle_category_code', 'seoul_middle_category_code_store_total', 'seoul_middle_category_code_ratio_to_the_entire_industry_ratio'])
-    quater_codes = set(seoul_df['year_quarter_code'])
+    # 서울 년분기별 총 점포수
+    year_quarter_code_groupby_all_store_counts = \
+        seoul_df.groupby('year_quarter_code')['store_count'].sum()
 
-    for quater_code in quater_codes:
-        # 중분류 코드 별 합 구하기
-        groupby_quater = seoul_df.loc[seoul_df['year_quarter_code'] == quater_code].groupby('middle_category_code').sum()
+    # 서울 년분기, 중분류 별 총 점포수
+    year_quarter_code_and_middle_category_code_groupby_sum_store_count = \
+        seoul_df.groupby(['year_quarter_code', 'middle_category_code'])['store_count'].sum()
 
-        # 서울시 분기별 점포수 합
-        seoul_all_stores = sum(seoul_df.loc[seoul_df['year_quarter_code'] == quater_code]['store_count'])
+    # sj :  j 업종 점포 개수/서울시 총 업종 점포 개수 → 업종의 종류만큼 도출
+    seoul_middle_category_code_ratio_to_the_entire_industry_ratio = year_quarter_code_and_middle_category_code_groupby_sum_store_count / year_quarter_code_groupby_all_store_counts
 
-        for idx, code in enumerate(groupby_quater.index):
-            row = {
-                'year_quarter_code': quater_code,
-                'middle_category_code': code,
-                'seoul_middle_category_code_store_total': groupby_quater.loc[code, 'store_count'],
-                'seoul_middle_category_code_ratio_to_the_entire_industry_ratio': groupby_quater.loc[code, 'store_count'] / seoul_all_stores
-            }
-            df = pd.concat([df, pd.DataFrame(row, index=[0])], ignore_index=True)
+    # 년분기, 상권코드 별 총 점포수
+    year_quarter_code_and_commercial_district_code_groupby_all_store_counts = \
+        store_df.groupby(['year_quarter_code', 'commercial_district_code'])['store_count'].sum()
 
-    # -----------------
-    quater_codes2 = set(store_df['year_quarter_code'])
-    df2 = pd.DataFrame(columns=['year_quarter_code','commercial_district_code', 'middle_category_code', 'middle_category_code_store_total', 'middle_category_code_ratio_to_the_entire_industry_ratio'])
-    for quater_code2 in quater_codes2:
-        print(quater_code2,"분기 시작")
-        for store_code in set(store_df.loc[store_df['year_quarter_code'] == quater_code2]['commercial_district_code']):
-            # 중분류 코드 별 합 구하기
-            groupby_quater2 = store_df.loc[(store_df['year_quarter_code'] == quater_code2) & (store_df['commercial_district_code'] == store_code)].groupby('middle_category_code').sum()
-            # 해당 상권에 없는 중분류 코드
-            empty_datas = set(category_codes.keys()) - set(store_df.loc[(store_df['year_quarter_code'] == quater_code2) & (store_df['commercial_district_code'] == store_code)]['middle_category_code'])
+    # 년분기, 상권코드, 중분류 별 총 점포수
+    year_quarter_code_and_commercial_district_code_and_middle_category_code_groupby_sum_store_count = \
+        store_df.groupby(['year_quarter_code', 'commercial_district_code', 'middle_category_code'])['store_count'].sum()
 
-            # 상권, 분기별 점포수 합
-            all_stores = sum(store_df.loc[(store_df['year_quarter_code'] == quater_code2) & (store_df['commercial_district_code'] == store_code)]['store_count'])
+    # Sij : i 상권 내 j 업종 점포 개수 / i 상권의 총 점포 개수 → 업종의 종류 * 상권의 개수만큼 도출
+    commercial_district_code_middle_category_code_ratio_to_the_entire_industry_ratio = year_quarter_code_and_commercial_district_code_and_middle_category_code_groupby_sum_store_count / year_quarter_code_and_commercial_district_code_groupby_all_store_counts
 
-            for idx, code in enumerate(groupby_quater2.index):
-                row = {
-                    'year_quarter_code': quater_code2,
-                    'commercial_district_code': store_code,
-                    'middle_category_code': code,
-                    'middle_category_code_store_total': groupby_quater2.loc[code, 'store_count'],
-                    'middle_category_code_ratio_to_the_entire_industry_ratio': groupby_quater2.loc[code, 'store_count'] / all_stores,
-                }
-                df2 = pd.concat([df2, pd.DataFrame(row, index=[0])], ignore_index=True)
+    df1 = commercial_district_code_middle_category_code_ratio_to_the_entire_industry_ratio.reset_index()
+    df2 = seoul_middle_category_code_ratio_to_the_entire_industry_ratio.reset_index()
 
-            for empty_code in empty_datas:
-                row = {
-                    'year_quarter_code': quater_code2,
-                    'commercial_district_code': store_code,
-                    'middle_category_code': empty_code,
-                    'middle_category_code_store_total': 0,
-                    'middle_category_code_ratio_to_the_entire_industry_ratio': 0,
-                }
-                df2 = pd.concat([df2, pd.DataFrame(row, index=[0])], ignore_index=True)
+    df = df1.merge(df2, how='left', on=['year_quarter_code', 'middle_category_code'])
+    df['rdi'] = abs(df['store_count_x'] - df[
+        'store_count_y'])
 
-    # --------------
+    result = ((df.groupby(['year_quarter_code', 'commercial_district_code'])['rdi'].sum()) ** -1).reset_index()
 
-    # 필요한 영역 컬럼들만 가져오기
-    area_df = area_df[['commercial_district_code','area_size']]
+    result_df = result_df.merge(result, how='left', on=['year_quarter_code',
+                                                        'commercial_district_code'])
+    # 점포 밀도 계산
+    area = area_df.groupby('commercial_district_code')[
+        'area_size'].sum().reset_index()
+    merged_df = year_quarter_code_and_commercial_district_code_groupby_all_store_counts.reset_index().merge(area,
+                                                                                                            how='left',
+                                                                                                            on='commercial_district_code')
+    merged_df['store_density'] = merged_df['store_count'] / merged_df[
+        'area_size']
 
-    # --------------
-    quater_codes = set(store_df['year_quarter_code'])
-    for rdi_quater_code in tqdm(quater_codes):
-        # 분기별 상권코드로 groupby
-        groupby_store_code = df2.loc[(df2['year_quarter_code'] == rdi_quater_code)].groupby('commercial_district_code')
-
-        sj_list = df.loc[df['year_quarter_code'] == rdi_quater_code][['middle_category_code', 'seoul_middle_category_code_ratio_to_the_entire_industry_ratio']]
-
-        for store_code, group in groupby_store_code:
-            total = 0
-            # 분기별 상권코드로 groupby 후 중분류_코드, seoul_middle_category_code_ratio_to_the_entire_industry_ratio 컬럼만 추출
-            sij_list = group[['middle_category_code', 'middle_category_code_store_total', 'middle_category_code_ratio_to_the_entire_industry_ratio']]
-
-            # 분기별 상퀀코드별 점포수 합
-            store_total = sij_list['middle_category_code_store_total'].sum()
-
-            for index, sij in sij_list.iterrows():
-                sj_total = sj_list[sj_list['middle_category_code'] == sij['middle_category_code']]['seoul_middle_category_code_ratio_to_the_entire_industry_ratio']
-                if not sj_total.empty:
-                    total += abs(sij['middle_category_code_ratio_to_the_entire_industry_ratio'] - sj_total.iloc[0])
-
-            # 업종다양성 rdi 계산
-            rdi = math.pow(total,-1)
-
-            area = area_df[area_df['commercial_district_code']==store_code]['area_size']
-            area_value = area.iloc[0] if not area.empty else 0
-
-            target_row_idx = result_df.index[(result_df['year_quarter_code'] == rdi_quater_code) & (result_df['commercial_district_code'] == store_code)]
-            result_df.loc[target_row_idx, 'rdi'] = rdi
-            # result_df.loc[target_row_idx, 'area_size'] = area_value
-            # result_df.loc[target_row_idx, 'total_store_count'] = store_total
-            result_df.loc[target_row_idx, 'store_density'] = store_total / area_value if area_value != 0 else 0
+    result_df = result_df.merge(merged_df, how='left', on=['year_quarter_code',
+                                                           'commercial_district_code'])
+    result_df = result_df.drop(columns=['store_count', 'area_size'])
 
     return result_df
