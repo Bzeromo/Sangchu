e91bf635b94127db2847ed763d5b9836dd05adea
윤정인 <liae100@naver.com>
2024-03-21 17:44:56 +0900

    Feat : 회귀 모델 개발

--- Regression Analysis/상권분석모델.py
+++ Regression Analysis/상권분석모델.py
@@ -106,18 +106,18 @@ results = sm.OLS(y, X).fit()
 print(results.summary())
 # print(merged_data)
 
-# 독립 변수(X) 리스트
-independent_variables = X.columns
+# # 독립 변수(X) 리스트
+# independent_variables = X.columns
 
-# 그래프 그릴 피규어 생성
-plt.figure(figsize=(20, 15))
+# # 그래프 그릴 피규어 생성
+# plt.figure(figsize=(20, 15))
 
-# 독립 변수별로 그래프 그리기
-for i, column in enumerate(independent_variables, 1):
-    plt.subplot(5, 5, i)  # 그래프 위치 지정
-    sns.regplot(x=X[column], y=y)  # regplot 그리기
-    plt.xlabel(column)  # x축 레이블 설정
-    plt.ylabel('monthly_sales_now')  # y축 레이블 설정
+# # 독립 변수별로 그래프 그리기
+# for i, column in enumerate(independent_variables, 1):
+#     plt.subplot(5, 5, i)  # 그래프 위치 지정
+#     sns.regplot(x=X[column], y=y)  # regplot 그리기
+#     plt.xlabel(column)  # x축 레이블 설정
+#     plt.ylabel('monthly_sales_now')  # y축 레이블 설정
 
-plt.tight_layout()  # 그래프 간격 조정
-plt.show()
+# plt.tight_layout()  # 그래프 간격 조정
+# plt.show()
