d4c63488b66d5e18d11a7144f84a7634f4bfda25
안성재 <sammy0329@naver.com>
2024-03-22 14:20:01 +0900

    Fix: 상권코드 불일치 제거 로직 수정

--- Backend/DataPreProcessing/modules/commercialDistrictCodePreProcessing.py
+++ Backend/DataPreProcessing/modules/commercialDistrictCodePreProcessing.py
@@ -17,62 +17,23 @@ def check_do_not_have_commercial_district_code(df_list):
     # 데이터프레임 이름을 키로 갖고, 데이터프레임을 값으로 갖는 딕셔너리 생성
     df_dict = dict(zip(df_names, df_list))
 
-    working_population_with_commercial_district_df_set = set(
-        df_dict['area_with_commercial_district_df']['commercial_district_code']) - set(
-        df_dict['working_population_with_commercial_district_df']['commercial_district_code'])
-    store_with_commercial_district_df_set = set(df_dict['area_with_commercial_district_df']['commercial_district_code']) - set(
-        df_dict['store_with_commercial_district_df']['commercial_district_code'])
-    sales_commercial_district_df_set = set(df_dict['area_with_commercial_district_df']['commercial_district_code']) - set(
-        df_dict['sales_commercial_district_df']['commercial_district_code'])
-    foot_traffic_with_commercial_district_df_set = set(
-        df_dict['area_with_commercial_district_df']['commercial_district_code']) - set(
-        df_dict['foot_traffic_with_commercial_district_df']['commercial_district_code'])
-    resident_population_with_commercial_district_df_set = set(
-        df_dict['area_with_commercial_district_df']['commercial_district_code']) - set(
-        df_dict['resident_population_with_commercial_district_df']['commercial_district_code'])
-    apartment_with_commercial_district_df_set = set(df_dict['area_with_commercial_district_df']['commercial_district_code']) - set(
-        df_dict['apartment_with_commercial_district_df']['commercial_district_code'])
-    facilities_with_commercial_district_df_set = set(
-        df_dict['area_with_commercial_district_df']['commercial_district_code']) - set(
-        df_dict['facilities_with_commercial_district_df']['commercial_district_code'])
-    income_consumption_with_commercial_district_df_set = set(
-        df_dict['area_with_commercial_district_df']['commercial_district_code']) - set(
-        df_dict['income_consumption_with_commercial_district_df']['commercial_district_code'])
-    commercial_district_change_indicator_with_commercial_district_df_set = set(
-        df_dict['area_with_commercial_district_df']['commercial_district_code']) - set(
-        df_dict['commercial_district_change_indicator_with_commercial_district_df']['commercial_district_code'])
+    # area_with_commercial_district_df의 상권 코드를 하나의 set으로 만듦
+    all_area_codes = set(df_dict['area_with_commercial_district_df']['commercial_district_code'])
 
-    # 각각 몇개의 상권이 비었는지 체크
-    print("working_population_with_commercial_district_df : ", working_population_with_commercial_district_df_set,
-          len(working_population_with_commercial_district_df_set))
-    print("store_with_commercial_district_df : ", store_with_commercial_district_df_set,
-          len(store_with_commercial_district_df_set))
-    print("sales_commercial_district_df : ", sales_commercial_district_df_set, len(sales_commercial_district_df_set))
-    print("foot_traffic_with_commercial_district_df : ", foot_traffic_with_commercial_district_df_set,
-          len(foot_traffic_with_commercial_district_df_set))
-    print("resident_population_with_commercial_district_df : ", resident_population_with_commercial_district_df_set,
-          len(resident_population_with_commercial_district_df_set))
-    print("apartment_with_commercial_district_df : ", apartment_with_commercial_district_df_set,
-          len(apartment_with_commercial_district_df_set))
-    print("facilities_with_commercial_district_df : ", facilities_with_commercial_district_df_set,
-          len(facilities_with_commercial_district_df_set))
-    print("income_consumption_with_commercial_district_df : ", income_consumption_with_commercial_district_df_set,
-          len(income_consumption_with_commercial_district_df_set))
-    print("commercial_district_change_indicator_with_commercial_district_df : ",
-          commercial_district_change_indicator_with_commercial_district_df_set,
-          len(commercial_district_change_indicator_with_commercial_district_df_set))
+    # 각 년분기별로 상권 코드의 set을 구하고, 차집합을 구한 뒤 합집합을 구하기
+    delete_codes = set()
 
-    delete_codes = working_population_with_commercial_district_df_set.union(store_with_commercial_district_df_set,
-                                                                            sales_commercial_district_df_set,
-                                                                            foot_traffic_with_commercial_district_df_set,
-                                                                            resident_population_with_commercial_district_df_set,
-                                                                            apartment_with_commercial_district_df_set,
-                                                                            facilities_with_commercial_district_df_set,
-                                                                            income_consumption_with_commercial_district_df_set,
-                                                                            commercial_district_change_indicator_with_commercial_district_df_set)
+    for key, df in df_dict.items():
+        if key == 'area_with_commercial_district_df':
+            continue
+        elif key == 'commercial_district_change_indicator_with_commercial_district_df':
+            delete_codes.update(all_area_codes - set(df['commercial_district_code']))
+        else:
+            delete_codes.update(set().union(*[all_area_codes - set(group['commercial_district_code']) for name, group in
+                                             df.groupby('year_quarter_code')]))
+        print(delete_codes)
+
     print("제거할 상권코드 : ", delete_codes, len(delete_codes))
-
-
 
     return delete_them_do_not_have_commercial_district_code(delete_codes, df_dict)
 
