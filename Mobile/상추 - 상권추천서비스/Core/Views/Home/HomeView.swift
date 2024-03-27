f76708cb5d73157868ebbf9c3aefc1207637c16c
양희태 <ht@yanghuitaeui-MacBookPro.local>
2024-03-27 22:37:31 +0900

    Style : main merge를 위해 약산 수정

--- Mobile/상추 - 상권추천서비스/Core/Views/Home/HomeView.swift
+++ Mobile/상추 - 상권추천서비스/Core/Views/Home/HomeView.swift
@@ -345,102 +345,107 @@ struct HomeView: View {
             }
             Divider().background(Color.gray.opacity(0.3)) // 절취선의 색상과 투명도를 설정합니다.
                 .padding(.leading, 20).padding(.top, 20)
+            
+            Section(header: HStack(alignment: .bottom){
+                Text("지역별 랭킹").font(.title2).fontWeight(.semibold).foregroundColor(.black).padding(.leading , 20)
+                Spacer()
+            }
                 // 하단 네비바들
-                VStack{
-                    HStack{
-                        Button(action: {}) {
-                            Image(uiImage: UIImage(named: "AppIcon.png")!)
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                                .foregroundColor(.black) // 버튼의 크기를 지정합니다.
-                        }
-                        Spacer()
-                        Button(action: {
-                            print("공백")
-                        }) {
-                            Text("") // 내용이 없는 텍스트를 제공합니다.
-                                .frame(maxWidth: .infinity, maxHeight: .infinity) // 버튼의 터치 가능 영역을 지정합니다.
-                                .background(Color.clear)
-                        }.frame(maxWidth: .infinity)
-                        Spacer()
-                        
-                        Button(action: {}) {
-                            Image(systemName: "magnifyingglass.circle")
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                                .foregroundColor(Color(hex: "93C73D")) // 버튼의 색상 지정합니다.
-                        }
-                    }
-                    .frame(maxWidth: UIScreen.main.bounds.width * 0.84, maxHeight : UIScreen.main.bounds.height * 0.02)
-                    .padding() // 내부 여백 추가
-                    .background(Color.white) // 배경색 추가
-                    .cornerRadius(70) // 모서리를 둥글게 처리
-                    .shadow(color: Color.black.opacity(0.1), radius: 2, x: 2, y: 2)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 70) // 둥근 모서리 설정
-                            .stroke(Color(hex: "93C73D"), lineWidth: 1) // 테두리 색상과 두께 지정
-                    ) // 검색창
-                    
-                    HStack{
-                        NavigationLink (destination: BDMapView(cameraLatitude : nil, cameraLongitude : nil)) {
-                            HStack {
-                                VStack (alignment: .leading) {
-                                    Text("상권 지도")
-                                        .font(.system(size: 22))
-                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                                        .foregroundColor(Color(hex: "767676"))
-                                        .padding(2)
-                                    Text("주변 상권 보기")
-                                        .font(.system(size: 12))
-                                        .foregroundColor(Color(hex: "93C73D"))
-                                        .padding(3)
-                                }
-                                Spacer()
-                                VStack{
-                                    Spacer()
-                                    Image(systemName: "map.circle")
-                                        .resizable()
-                                        .frame(width: 30, height: 30)
-                                        .foregroundColor(Color(hex: "93C73D")) // 버튼의 크기를 지정합니다.
-                                }
-                            }
-                            .frame(width: UIScreen.main.bounds.width * 0.38)
-                            .padding()
-                            .background(Color.white) // 전체 VStack의 배경색 설정
-                            .cornerRadius(20) // 모서리 둥글게
-                            .shadow(color: Color.black.opacity(0.2), radius: 1, x: 1, y: 1) // 그림자 효과 추가
-                        }
-                        
-                        NavigationLink (destination: BookMarkList()) {
-                            HStack {
-                                VStack (alignment: .leading) {
-                                    Text("북마크")
-                                        .font(.system(size: 22))
-                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                                        .foregroundColor(Color(hex: "767676"))
-                                        .padding(2)
-                                    Text(" 개")
-                                        .font(.system(size: 12))
-                                        .foregroundColor(Color(hex: "93C73D"))
-                                        .padding(3)
-                                }
-                                Spacer()
-                                VStack{
-                                    Spacer()
-                                    Image(systemName: "bookmark.circle")
-                                        .resizable()
-                                        .frame(width: 30, height: 30)
-                                        .foregroundColor(Color(hex: "93C73D"))
-                                }
-                            }.frame(width: UIScreen.main.bounds.width * 0.38)
-                                .padding()
-                                .background(Color.white) // 전체 VStack의 배경색 설정
-                                .cornerRadius(20) // 모서리 둥글게
-                                .shadow(color: Color.black.opacity(0.2), radius: 1, x: 1, y: 1) // 그림자 효과 추가
-                        }
-                    } // 지도탭과 북마크탭
-                    
-                }
+//                VStack{
+//                    HStack{
+//                        Button(action: {}) {
+//                            Image(uiImage: UIImage(named: "AppIcon.png")!)
+//                                .resizable()
+//                                .frame(width: 30, height: 30)
+//                                .foregroundColor(.black) // 버튼의 크기를 지정합니다.
+//                        }
+//                        Spacer()
+//                        Button(action: {
+//                            print("공백")
+//                        }) {
+//                            Text("") // 내용이 없는 텍스트를 제공합니다.
+//                                .frame(maxWidth: .infinity, maxHeight: .infinity) // 버튼의 터치 가능 영역을 지정합니다.
+//                                .background(Color.clear)
+//                        }.frame(maxWidth: .infinity)
+//                        Spacer()
+//                        
+//                        Button(action: {}) {
+//                            Image(systemName: "magnifyingglass.circle")
+//                                .resizable()
+//                                .frame(width: 30, height: 30)
+//                                .foregroundColor(Color(hex: "93C73D")) // 버튼의 색상 지정합니다.
+//                        }
+//                    }
+//                    .frame(maxWidth: UIScreen.main.bounds.width * 0.84, maxHeight : UIScreen.main.bounds.height * 0.02)
+//                    .padding() // 내부 여백 추가
+//                    .background(Color.white) // 배경색 추가
+//                    .cornerRadius(70) // 모서리를 둥글게 처리
+//                    .shadow(color: Color.black.opacity(0.1), radius: 2, x: 2, y: 2)
+//                    .overlay(
+//                        RoundedRectangle(cornerRadius: 70) // 둥근 모서리 설정
+//                            .stroke(Color(hex: "93C73D"), lineWidth: 1) // 테두리 색상과 두께 지정
+//                    ) // 검색창
+//                    
+//                    HStack{
+//                        NavigationLink (destination: BDMapView(cameraLatitude : nil, cameraLongitude : nil)) {
+//                            HStack {
+//                                VStack (alignment: .leading) {
+//                                    Text("상권 지도")
+//                                        .font(.system(size: 22))
+//                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+//                                        .foregroundColor(Color(hex: "767676"))
+//                                        .padding(2)
+//                                    Text("주변 상권 보기")
+//                                        .font(.system(size: 12))
+//                                        .foregroundColor(Color(hex: "93C73D"))
+//                                        .padding(3)
+//                                }
+//                                Spacer()
+//                                VStack{
+//                                    Spacer()
+//                                    Image(systemName: "map.circle")
+//                                        .resizable()
+//                                        .frame(width: 30, height: 30)
+//                                        .foregroundColor(Color(hex: "93C73D")) // 버튼의 크기를 지정합니다.
+//                                }
+//                            }
+//                            .frame(width: UIScreen.main.bounds.width * 0.38)
+//                            .padding()
+//                            .background(Color.white) // 전체 VStack의 배경색 설정
+//                            .cornerRadius(20) // 모서리 둥글게
+//                            .shadow(color: Color.black.opacity(0.2), radius: 1, x: 1, y: 1) // 그림자 효과 추가
+//                        }
+//                        
+//                        NavigationLink (destination: BookMarkList()) {
+//                            HStack {
+//                                VStack (alignment: .leading) {
+//                                    Text("북마크")
+//                                        .font(.system(size: 22))
+//                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+//                                        .foregroundColor(Color(hex: "767676"))
+//                                        .padding(2)
+//                                    Text(" 개")
+//                                        .font(.system(size: 12))
+//                                        .foregroundColor(Color(hex: "93C73D"))
+//                                        .padding(3)
+//                                }
+//                                Spacer()
+//                                VStack{
+//                                    Spacer()
+//                                    Image(systemName: "bookmark.circle")
+//                                        .resizable()
+//                                        .frame(width: 30, height: 30)
+//                                        .foregroundColor(Color(hex: "93C73D"))
+//                                }
+//                            }.frame(width: UIScreen.main.bounds.width * 0.38)
+//                                .padding()
+//                                .background(Color.white) // 전체 VStack의 배경색 설정
+//                                .cornerRadius(20) // 모서리 둥글게
+//                                .shadow(color: Color.black.opacity(0.2), radius: 1, x: 1, y: 1) // 그림자 효과 추가
+//                        }
+//                    } // 지도탭과 북마크탭
+//                    
+//                }
             
             
             
